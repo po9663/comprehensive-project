599649b4b60f5fba0537dec383dc515b2c405b8c
JONGOAN\jongoan <kenu12@Naver.com>
2020-04-07 10:31:12 +0900

    키보드 조작

---
+++ New Unity Project/Assets/Scenes/Materials/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    
+
+    public float moveSpeed = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Debug.Log(moveHorizontal + "===" + moveVertical);
+            Vector3 moveVector = new Vector3(moveHorizontal, moveVertical, 0.1f);
+            transform.position += moveVector * moveSpeed * Time.deltaTime;
+
+        
+        
+    }
+}
