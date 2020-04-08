f4324c4d6c6c1795941436ca9e2bf74a8c019f3a
kwonwoo\kwonwoo <kenu12@Naver.com>
2020-04-09 05:58:26 +0900

    키보드 y축이동 q,e 랜덤으로 상자 나오기

--- New Unity Project/Assets/Scenes/Materials/NewBehaviourScript.cs
+++ New Unity Project/Assets/Scenes/Materials/NewBehaviourScript.cs
@@ -5,25 +5,96 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     
+    
+    public float moveSpeed = 10.0f;
     // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
+        Debug.Log("awake");
+    }
+    
+    IEnumerator Start()
+    {
+        Debug.Log("start");
+        yield return new WaitForSeconds(1);
+        int rand = 0;
+        GameObject[] boxes;
+
+        boxes = GameObject.FindGameObjectsWithTag("1");
+        for (int i = 0; i < boxes.Length; i++)
+        {
+            boxes[i].SetActive(false);
+            Debug.Log(i + "번째" + boxes[i]);
+        }
+        rand = Random.Range(0, 5);
+        boxes[rand].SetActive(true);
     }
     
 
-    public float moveSpeed = 10.0f;
+    
+    
     // Update is called once per frame
     void Update()
     {
-        
-        
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-            Debug.Log(moveHorizontal + "===" + moveVertical);
-            Vector3 moveVector = new Vector3(moveHorizontal, moveVertical, 0.1f);
-            transform.position += moveVector * moveSpeed * Time.deltaTime;
+
 
         
-        
+
+        /*
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = 0.0f;
+        float moveZ = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            moveY += 0.5f;
+            Vector3 moveVector = new Vector3(moveX, moveY, moveZ);
+            transform.position += moveVector * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            moveY -= 1.0f;
+            Vector3 moveVector = new Vector3(moveX, moveY, moveZ);
+            transform.position += moveVector * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            Vector3 moveVector = new Vector3(moveX, moveY, moveZ);
+            transform.position += moveVector * moveSpeed * Time.deltaTime;
+        }
+
+        */
+        moving();
     }
+    
+    
+    void moving()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = 0.0f;
+        float moveZ = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            moveY += 0.5f;
+            Vector3 moveVector = new Vector3(moveX, moveY, moveZ);
+            transform.position += moveVector * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            moveY -= 1.0f;
+            Vector3 moveVector = new Vector3(moveX, moveY, moveZ);
+            transform.position += moveVector * moveSpeed * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+        else
+        {
+            Vector3 moveVector = new Vector3(moveX, moveY, moveZ);
+            transform.position += moveVector * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    
 }
