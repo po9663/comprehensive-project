f4bba45fc3744f2e9337f43ef5c93bf423e2c9bd
kwonwoo\kwonwoo <kenu12@Naver.com>
2020-04-20 19:30:00 +0900

    스페이스바 정지, y축으로 중력

--- finalproject/Assets/BlockFactory.cs
+++ finalproject/Assets/BlockFactory.cs
@@ -10,27 +10,33 @@ public class BlockFactory {
 	GameObject[] blockTypes;
 	Material[] materialTypes;
 
-	public BlockFactory(GameObject a, GameObject b, GameObject c, GameObject d,
-	                    Material e, Material f, Material g) {
-		blockTypes = new GameObject[4];
+	public BlockFactory(GameObject a, GameObject b, GameObject c, GameObject d, GameObject e, GameObject f, GameObject g, GameObject h, GameObject i, GameObject j,
+                        Material k, Material l, Material m) {
+		blockTypes = new GameObject[10];
 		blockTypes[0] = a;
 		blockTypes[1] = b;
 		blockTypes[2] = c;
 		blockTypes[3] = d;
+        blockTypes[4] = e;
+        blockTypes[5] = f;
+        blockTypes[6] = g;
+        blockTypes[7] = h;
+        blockTypes[8] = i;
+        blockTypes[9] = j;
 
-		materialTypes = new Material[4];
-		materialTypes[0] = e;
-		materialTypes[1] = f;
-		materialTypes[3] = g;
+        materialTypes = new Material[4];
+		materialTypes[0] = k;
+		materialTypes[1] = l;
+		materialTypes[3] = m;
 	}
 
 	public GameObject GetNextBlock() {	
-		int num = ((int)Mathf.Round (Random.value * 10000)) % 4;
+		int num = ((int)Mathf.Round (Random.value * 10000)) % 10;
 		GameObject block = blockTypes[num];
 		num = ((int)Mathf.Round (Random.value * 10000)) % 3;
 		foreach (MeshRenderer mr in block.GetComponentsInChildren<MeshRenderer>())
 		{
-			Debug.Log("here");
+			Debug.Log("BlockFactory here");
 			mr.material = materialTypes[num];
 		}
 		return block;
--- finalproject/Assets/BlockMovement.cs
+++ finalproject/Assets/BlockMovement.cs
@@ -1,57 +1,74 @@
-﻿using UnityEngine;
+﻿/*
+
+
+
+
+*/
+
+using UnityEngine;
 using System.Collections;
 
 public class BlockMovement : MonoBehaviour {
 
-	public GameObject BLOCKSMALL;
-	public GameObject BLOCKSTICK;
-	public GameObject BLOCKL;
-	public GameObject BLOCKSQUARE;
-
+	public GameObject Block;
+    public GameObject Block2;
+    public GameObject Block3;
+    public GameObject Block4;
+    public GameObject Block5;
+    public GameObject Block6;
+    public GameObject Block7;
+    public GameObject Block8;
+    public GameObject Block9;
+    public GameObject Block10;
+    
 	public Material MATRED;
 	public Material MATBLUE;
 	public Material MATGREEN;
 
+    int passnum = 0;
 
 	BlockFactory blockFactory;
 	GameObject activeBlock;
 
-	float moveSpeed = 10f;
-	float defaultFallSpeed = 1f;
-    float fallSpeed = 1f;
-
-	float zNextCheckPoint = 1f;
-
-    bool isFalling = true;
+	float moveSpeed = 10f; 
+	//float defaultFallSpeed = 1f;
+    //float fallSpeed = 1f;
+    
+	//float zNextCheckPoint = 1f;
+    //bool isFalling = true;
     bool isMoving = false;
     bool isRotating = false;
     Vector3 blockStartPos, blockEndPos;
     Quaternion blockStartRot, blockEndRot;
-
     float tElapsed;
-
 	// Use this for initialization
 	void Start () {
-		blockFactory = new BlockFactory(BLOCKSMALL, BLOCKSTICK, BLOCKL, BLOCKSQUARE
-		                                , MATRED, MATBLUE, MATGREEN);
-		initGrid();
+        // BlockFactory 클래스 객체생성
+		blockFactory = new BlockFactory(Block, Block2, Block3, Block4, Block5, Block6, Block7, Block8, Block9, Block10
+                                        , MATRED, MATBLUE, MATGREEN);
+        //grid생성
+        initGrid();
         // get the active block
         //activeBlock = GameObject.FindGameObjectWithTag("Player");
+        //상자 생성 위치는 0,0,1위치
 		activeBlock = (GameObject)GameObject.Instantiate(blockFactory.GetNextBlock(), new Vector3(0, 0, 1), Quaternion.identity);
-		activeBlock.transform.position = new Vector3(0, 0, 1);
+		activeBlock.transform.position = new Vector3(0, -2, 1);
 		foreach (MeshRenderer mr in activeBlock.GetComponentsInChildren<MeshRenderer>())
 		{
-			Debug.Log("here");
+			Debug.Log("Start here");
 			mr.material = MATBLUE;
 		}
 
 	}
+    
+    
 
     // Update is called once per frame
     void Update()
     {
         if (!isMoving && !isRotating)
         {
+            
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
 				Vector3 futurePos = activeBlock.transform.position + new Vector3(-1, 0, 0);
@@ -70,7 +87,7 @@ public class BlockMovement : MonoBehaviour {
             }
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 1, 0);
+				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 0, 1);
 				Quaternion futureRot = activeBlock.transform.rotation;
 				if (!IsPositionBlocked(futurePos, futureRot)) {
 					SmoothMove(activeBlock.transform.position, futurePos);
@@ -78,12 +95,69 @@ public class BlockMovement : MonoBehaviour {
             }
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, -1, 0);
+				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 0, -1);
 				Quaternion futureRot = activeBlock.transform.rotation;
 				if (!IsPositionBlocked(futurePos, futureRot)) {
 					SmoothMove(activeBlock.transform.position, futurePos);
 				}
             }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 0, 1);
+                Quaternion futureRot = activeBlock.transform.rotation;
+                if (!IsPositionBlocked(futurePos, futureRot))
+                {
+                    SmoothMove(activeBlock.transform.position, futurePos);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 0, -1);
+                Quaternion futureRot = activeBlock.transform.rotation;
+                if (!IsPositionBlocked(futurePos, futureRot))
+                {
+                    SmoothMove(activeBlock.transform.position, futurePos);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                Vector3 futurePos = activeBlock.transform.position + new Vector3(-1, 0, 0);
+                Quaternion futureRot = activeBlock.transform.rotation;
+                if (!IsPositionBlocked(futurePos, futureRot))
+                {
+                    SmoothMove(activeBlock.transform.position, futurePos);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                Vector3 futurePos = activeBlock.transform.position + new Vector3(1, 0, 0);
+                Quaternion futureRot = activeBlock.transform.rotation;
+                if (!IsPositionBlocked(futurePos, futureRot))
+                {
+                    SmoothMove(activeBlock.transform.position, futurePos);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 1, 0);
+                Quaternion futureRot = activeBlock.transform.rotation;
+                if (!IsPositionBlocked(futurePos, futureRot))
+                {
+                    SmoothMove(activeBlock.transform.position, futurePos);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                Vector3 futurePos = activeBlock.transform.position + new Vector3(0, -1, 0);
+                Quaternion futureRot = activeBlock.transform.rotation;
+                if (!IsPositionBlocked(futurePos, futureRot))
+                {
+                    SmoothMove(activeBlock.transform.position, futurePos);
+                }
+            }
+
+
+            /*
 			else if (Input.GetKeyDown(KeyCode.Q))
 			{
 				Vector3 futurePos = activeBlock.transform.position;
@@ -132,12 +206,20 @@ public class BlockMovement : MonoBehaviour {
 					SmoothRotate(activeBlock.transform.rotation, futureRot);
 				}
 			}
+            */
 
-			fallSpeed = defaultFallSpeed;
-			if (Input.GetKey(KeyCode.Space)) 
+
+            //fallSpeed = defaultFallSpeed;
+
+            /*
+			else if (Input.GetKey(KeyCode.Space)) 
 			{
-				fallSpeed = 20f;
-			}
+                Debug.Log("space");
+
+                
+                //fallSpeed = 20f;
+            }
+            */
 			//Debug.Log (fallSpeed);
         }
         else
@@ -151,58 +233,85 @@ public class BlockMovement : MonoBehaviour {
                 SmoothRotate();
             }
         }
-        if (isFalling)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-			activeBlock.transform.position += new Vector3(0, 0, fallSpeed * Time.deltaTime);
-			if (activeBlock.transform.position.z >= zNextCheckPoint) {
-				zNextCheckPoint++;
+            Debug.Log("This-space-");
+            /*
+            Debug.Log("----------------------------------------------------------------------------");
+            
+            Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 0, 1);
+            Quaternion futureRot = activeBlock.transform.rotation;
+            Debug.Log("###futurePos++" + futurePos + "###futureRot++" + futureRot);
+            if (IsPositionBlocked(futurePos, futureRot))
+            {
+                Debug.Log("This-isFalling-");
+                activeBlock.transform.position = new Vector3(
+                    (int)Mathf.Round(activeBlock.transform.position.x),
+                    (int)Mathf.Round(activeBlock.transform.position.y),
+                    (int)Mathf.Round(activeBlock.transform.position.z));
+                Debug.Log("isFalling in -:" + (int)Mathf.Round(activeBlock.transform.position.x) +
+                    (int)Mathf.Round(activeBlock.transform.position.y) +
+                    (int)Mathf.Round(activeBlock.transform.position.z));
+                SetPositionBlocked();
 
-				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 0, 1);
-				Quaternion futureRot = activeBlock.transform.rotation;
-				if (IsPositionBlocked(futurePos, futureRot))
-				{
-					activeBlock.transform.position = new Vector3(
-						(int)Mathf.Round(activeBlock.transform.position.x), 
-						(int)Mathf.Round(activeBlock.transform.position.y), 
-						(int)Mathf.Round(activeBlock.transform.position.z));
-					SetPositionBlocked();
-
-					activeBlock = (GameObject)GameObject.Instantiate(blockFactory.GetNextBlock(), new Vector3(0, 0, 1), Quaternion.identity);
-					zNextCheckPoint = 1;
-				}
-			}
+                activeBlock = (GameObject)GameObject.Instantiate(blockFactory.GetNextBlock(), new Vector3(0, -2, 1), Quaternion.identity);
+                zNextCheckPoint = 1;
+            }
+            */
+            activeBlock.transform.position = new Vector3(
+                       (int)Mathf.Round(activeBlock.transform.position.x),
+                       (int)Mathf.Round(activeBlock.transform.position.y),
+                       (int)Mathf.Round(activeBlock.transform.position.z));
+            SetPositionBlocked();
+            
+            activeBlock = (GameObject)GameObject.Instantiate(blockFactory.GetNextBlock(), new Vector3(0, -2, 1), Quaternion.identity);
+            
+            
         }
 
     }
 
-	bool[,,] blocked = new bool[7,7,12];
+    
 
+    bool[,,] blocked = new bool[7,7,12];
 	private void initGrid() {
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 7; j++) {
 				for (int k = 0; k < 12; k++) {
+                    
 					blocked[i,j,k] = false;
+                    
 					if (i == 0 || j == 0 || k == 0 || i == 6 || j == 6 || k == 11)
 					{
 						blocked[i,j,k] = true;
-					}
+                    }
+                    
 				}
 			}
 		}
-	}
+    }
+    
 
 	private bool IsPositionBlocked(Vector3 futurePos, Quaternion futureRot) {
 		foreach (Transform cube in activeBlock.transform.GetComponentsInChildren<Transform>())
 		{
 			if (cube.childCount == 0)
 			{
-				//Debug.Log ("Cube Pos: " + cube.position);
-				//Debug.Log ("Future Pos: " + futurePos);
-				if (blocked[
-				            (int)Mathf.Round (futurePos.x + (cube.position.x - activeBlock.transform.position.x)) + 3, 
-				            (int)Mathf.Round (futurePos.y + (cube.position.y - activeBlock.transform.position.y)) + 3, 
-				            (int)Mathf.Round (futurePos.z + (cube.position.z - activeBlock.transform.position.z))])
-					return true;
+                
+                //Debug.Log ("Cube Pos: " + cube.position);
+                //Debug.Log ("Future Pos: " + futurePos);
+
+               
+                if (blocked[
+                            (int)Mathf.Round(futurePos.x + (cube.position.x - activeBlock.transform.position.x)) + 3,
+                            (int)Mathf.Round(futurePos.y + (cube.position.y - activeBlock.transform.position.y)) + 3,
+                            (int)Mathf.Round(futurePos.z + (cube.position.z - activeBlock.transform.position.z))])
+                {
+                    
+                    return true;
+                }
+                        
+                
 			}
 		}
 		return false;
@@ -219,10 +328,10 @@ public class BlockMovement : MonoBehaviour {
 		{
 			if (cube.childCount == 0)
 			{
-				int i = (int)Mathf.Round (cube.transform.position.x) + 3;
+                int i = (int)Mathf.Round (cube.transform.position.x) + 3;
 				int j = (int)Mathf.Round (cube.transform.position.y) + 3;
 				int k = (int)Mathf.Round (cube.transform.position.z);
-				blocked[i,j,k] = true;
+                blocked[i,j,k] = true;
 			}
 		}
 
@@ -259,4 +368,5 @@ public class BlockMovement : MonoBehaviour {
         activeBlock.transform.rotation = Quaternion.Lerp(blockStartRot, blockEndRot, tElapsed);
         if (tElapsed >= 1f) isRotating = false;
     }
+    
 }
