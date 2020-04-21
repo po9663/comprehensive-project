86aa57f6ebfe9b3eda4de1efd7e4d6cfcff76644
sn00918 <sn00918@naver.com>
2020-04-21 09:50:00 +0900

    ml-agents example

---
+++ ml/Assets/Scripts/BallAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class BallAgent : Agent
+{
+    private Rigidbody ballRigidbody; // 볼의 리지드바디
+    public Transform pivotTransform; // 위치의 기준점
+    public Transform target; // 아이템 목표
+    
+    public float moveforce = 10f; // 이동 힘
+    private bool targetEaten = false;
+    private bool dead = false;
+
+    void Awake()
+    {
+        ballRigidbody = GetComponent<Rigidbody>();
+    }
+}
