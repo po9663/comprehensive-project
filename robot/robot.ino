35d9e74734cb638128a29c213a23e88a16554e3f
HO <wnghtmd99@naver.com>
2020-06-22 16:19:30 +0900

    과정

--- robot/robot.ino
+++ robot/robot.ino
@@ -14,18 +14,21 @@ void loop() {
   if (Serial.available()) {
     
     
-    String ist = Serial.readStringUntil('\n');
+    String ist = Serial.readString();
     //좌표에 따른 매핑
     int oneComma = ist.indexOf(','); //콤마(,)를 찾아 번호를 매긴다
-    int twoComma = ist.indexOf(",",comma+1);
-   
+    int twoComma = ist.indexOf(",",oneComma+1);
+    int threeComma = ist.indexOf(",",oneComma+2);
+    
   
     int len = ist.length(); //문자의길이를 측정한다
-  
+  Serial.write(len);
     int x = ist.substring(0,oneComma).toInt(); //첫번째문자부터 콤마전까지의 수를 정수로 변환
-    int y = ist.substring(comma+1,towComma).toInt(); //콤마이후의 수를 정수로 변환
-    int z = ist.substring(twoComma+1,len).toInt(); //첫번째문자부터 콤마전까지의 수를 정수로 변환
-    
+    int y = ist.substring(oneComma+1,twoComma).toInt(); //콤마이후의 수를 정수로 변환
+    int z = ist.substring(twoComma+1).toInt(); //첫번째문자부터 콤마전까지의 수를 정수로 변환
+    Serial.write(x);
+    Serial.write(y);
+    Serial.write(z);
    //좌표값 유니티에서 받아옴
    /*
    if(x==0){
@@ -120,7 +123,7 @@ void loop() {
    
    */
 
-    int rx = constrain(map(x,0,180,150,600),150,600); //입력받은 x값을 매핑, contrain으로 범위를 한정
+   /* int rx = constrain(map(x,0,180,150,600),150,600); //입력받은 x값을 매핑, contrain으로 범위를 한정
   
     int ry = constrain(map(y,0,180,150,600),150,600); //위와같음
    
@@ -148,7 +151,7 @@ void loop() {
     Serial.print(ry);
     Serial.print(',');
     Serial.print(rz);
-    Serial.println(')');
+    Serial.println(')');*/
     
     
   }
