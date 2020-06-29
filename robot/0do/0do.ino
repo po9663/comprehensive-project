0b623ea000d196acb538d80c9f1ab36c68819c03
sn00918 <sn00918@naver.com>
2020-06-29 15:42:27 +0900

    복구

--- robot/0do/0do.ino
+++ robot/0do/0do.ino
@@ -11,20 +11,26 @@ void setup() {
 
 void loop() {
 if(Serial.available()){
+  int dd=1;
   String a= Serial.readString();
+  String b1="1,2,3,2,1,2/1,3,2,1,2,2/";
+
+ if(b1.compareTo(a)==1){
+  Serial.write(dd);
+ }
   
-  char re[4];
+  /*char re[4];
   a.substring(0,3).toCharArray(re,4);
   int b= atoi(re);
-  Serial.write(b);
-    for(int j=0; j<=b; j++){
+  
+    for(int j=2; j<=b; j++){
   servo.write(j);
   delay(20);
     }
-   for(int m=b; m>=0; m--){
+   for(int m=b; m>=2; m--){
   servo.write(m);
   delay(20);
-   }
+   }*/
   
 }
 
